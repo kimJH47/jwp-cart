89fc902f1be39cbef6d11614df5ee73e793e89f8
kimJaeHyeon <kmr2644@gmail.com>
2023-05-14 07:34:56 +0900

    test: item update test 작성

--- src/test/java/cart/repository/ItemRepositoryTest.java
+++ src/test/java/cart/repository/ItemRepositoryTest.java
@@ -2,7 +2,9 @@ package cart.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import cart.domain.Item;
 import cart.dto.ItemSaveRequest;
+import cart.dto.ItemUpdateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,6 +31,24 @@ class ItemRepositoryTest {
         long actual = itemRepository.save(itemSaveRequest);
         //then
         assertThat(actual).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("ItemUpdateRequest 를 받아서 성공적으로 업데이트 후 저장된 pk 값을 반환해야한다.")
+    void update(){
+        //given
+        ItemSaveRequest itemSaveRequest = new ItemSaveRequest("바나나", "ignore", 2000);
+        long id = itemRepository.save(itemSaveRequest);
+        ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(id, "사과", "https://chat.openai.com", 10000);
+        //when
+        long update = itemRepository.update(itemUpdateRequest);
+        Item actual = itemRepository.findAll().get(0);
+        //then
+        assertThat(actual.getId()).isEqualTo(update);
+        assertThat(actual.getName()).isEqualTo(itemUpdateRequest.getName());
+        assertThat(actual.getPrice()).isEqualTo(itemUpdateRequest.getPrice());
+        assertThat(actual.getImageUrl()).isEqualTo(itemUpdateRequest.getImageUrl());
+
     }
 
 }
