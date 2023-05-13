f7a34db41f8642c1a4788a8d95fc38ba142cadae
kimJaeHyeon <kmr2644@gmail.com>
2023-05-14 07:43:55 +0900

    fix: save 테스트 방법 변경

--- src/test/java/cart/repository/ItemRepositoryTest.java
+++ src/test/java/cart/repository/ItemRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import cart.domain.Item;
 import cart.dto.ItemSaveRequest;
 import cart.dto.ItemUpdateRequest;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,22 +23,28 @@ class ItemRepositoryTest {
     void init() {
         itemRepository = new ItemRepository(jdbcTemplate);
     }
+
     @Test
     @DisplayName("ItemSaveRequest 를 받아서 성공적으로 저장 후 저장된 pk 값을 반환해야한다.")
-    void save(){
+    void save() {
         //given
         ItemSaveRequest itemSaveRequest = new ItemSaveRequest("바나나", "ignore", 2000);
         //when
-        long actual = itemRepository.save(itemSaveRequest);
+        long id = itemRepository.save(itemSaveRequest);
+        Item actual = itemRepository.findAll().get(0);
         //then
-        assertThat(actual).isEqualTo(1);
+        assertThat(actual.getId()).isEqualTo(id);
+        assertThat(actual.getName()).isEqualTo(itemSaveRequest.getName());
+        assertThat(actual.getPrice()).isEqualTo(itemSaveRequest.getPrice());
+        assertThat(actual.getImageUrl()).isEqualTo(itemSaveRequest.getImageUrl());
+
     }
 
     @Test
     @DisplayName("ItemUpdateRequest 를 받아서 성공적으로 업데이트 후 저장된 pk 값을 반환해야한다.")
-    void update(){
+    void update() {
         //given
-        ItemSaveRequest itemSaveRequest = new ItemSaveRequest("바나나", "ignore", 2000);
+        ItemSaveRequest itemSaveRequest = new ItemSaveRequest("망고", "ignore", 30000);
         long id = itemRepository.save(itemSaveRequest);
         ItemUpdateRequest itemUpdateRequest = new ItemUpdateRequest(id, "사과", "https://chat.openai.com", 10000);
         //when
@@ -48,6 +55,20 @@ class ItemRepositoryTest {
         assertThat(actual.getName()).isEqualTo(itemUpdateRequest.getName());
         assertThat(actual.getPrice()).isEqualTo(itemUpdateRequest.getPrice());
         assertThat(actual.getImageUrl()).isEqualTo(itemUpdateRequest.getImageUrl());
+    }
+
+    @Test
+    @DisplayName("item pk 를 인자로 받아 성공적으로 삭제 후 삭제된 pk 가 반환되어야한다.")
+    void delete() {
+        //given
+        ItemSaveRequest itemSaveRequest = new ItemSaveRequest("포도", "ignore", 5000);
+        long expected = itemRepository.save(itemSaveRequest);
+        //when
+        long actual = itemRepository.delete(expected);
+        List<Item> items = itemRepository.findAll();
+        //then
+        assertThat(actual).isEqualTo(expected);
+        assertThat(items).isEmpty();
 
     }
 
