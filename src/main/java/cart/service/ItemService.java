bc248addd66bf9b0d30e9b4ba262e27c82bf6200
kimJaeHyeon <kmr2644@gmail.com>
2023-05-14 10:58:06 +0900

    feat: item crud service 구현

---
+++ src/main/java/cart/service/ItemService.java
@@ -0,0 +1,36 @@
+package cart.service;
+
+
+import cart.domain.Item;
+import cart.dto.ItemSaveRequest;
+import cart.dto.ItemUpdateRequest;
+import cart.repository.ItemRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public long save(ItemSaveRequest itemSaveRequest) {
+        return itemRepository.save(itemSaveRequest);
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    public long update(ItemUpdateRequest itemUpdateRequest) {
+        return itemRepository.update(itemUpdateRequest);
+    }
+
+    public long delete(long id) {
+        return itemRepository.delete(id);
+    }
+
+}
