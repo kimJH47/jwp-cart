0b9ab58a3313927cea2a94a4c9488b50cf42bce1
kimJaeHyeon <kmr2644@gmail.com>
2023-05-21 05:43:42 +0900

    style: 코드 정렬

--- src/main/java/cart/repository/ItemRepository.java
+++ src/main/java/cart/repository/ItemRepository.java
@@ -1,56 +1,58 @@
 package cart.repository;
 
-import cart.domain.Item;
-import cart.dto.request.ItemSaveRequest;
-import cart.dto.request.ItemUpdateRequest;
 import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import cart.domain.Item;
+import cart.dto.request.ItemSaveRequest;
+import cart.dto.request.ItemUpdateRequest;
+
 @Repository
 public class ItemRepository {
-    private final SimpleJdbcInsert simpleJdbcInsert;
-    private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcInsert simpleJdbcInsert;
+	private final JdbcTemplate jdbcTemplate;
 
-    public ItemRepository(JdbcTemplate jdbcTemplate) {
-        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("ITEM_INFO")
-                .usingGeneratedKeyColumns("ID");
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	public ItemRepository(JdbcTemplate jdbcTemplate) {
+		this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+			.withTableName("ITEM_INFO")
+			.usingGeneratedKeyColumns("ID");
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    public long save(ItemSaveRequest itemSaveRequest) {
-        return simpleJdbcInsert.executeAndReturnKey(new BeanPropertySqlParameterSource(itemSaveRequest))
-                .longValue();
-    }
+	public long save(ItemSaveRequest itemSaveRequest) {
+		return simpleJdbcInsert.executeAndReturnKey(new BeanPropertySqlParameterSource(itemSaveRequest))
+			.longValue();
+	}
 
-    public long update(ItemUpdateRequest itemUpdateRequest) {
-        String sql = "UPDATE ITEM_INFO set name = ?, image_url = ?, price = ? where id = ?";
-        jdbcTemplate.update(sql, itemUpdateRequest.getName(),
-                itemUpdateRequest.getImageUrl(), itemUpdateRequest.getPrice(), itemUpdateRequest.getId());
-        return itemUpdateRequest.getId();
-    }
+	public long update(ItemUpdateRequest itemUpdateRequest) {
+		String sql = "UPDATE ITEM_INFO set name = ?, image_url = ?, price = ? where id = ?";
+		jdbcTemplate.update(sql, itemUpdateRequest.getName(),
+			itemUpdateRequest.getImageUrl(), itemUpdateRequest.getPrice(), itemUpdateRequest.getId());
+		return itemUpdateRequest.getId();
+	}
 
-    public List<Item> findAll() {
-        String sql = "select * from ITEM_INFO";
-        return jdbcTemplate.query(sql, itemRowMapper());
-    }
+	public List<Item> findAll() {
+		String sql = "select * from ITEM_INFO";
+		return jdbcTemplate.query(sql, itemRowMapper());
+	}
 
-    private RowMapper<Item> itemRowMapper() {
-        return (rs, rowNum) -> new Item(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getInt("price"),
-                rs.getString("image_url")
-        );
-    }
+	private RowMapper<Item> itemRowMapper() {
+		return (rs, rowNum) -> new Item(
+			rs.getLong("id"),
+			rs.getString("name"),
+			rs.getInt("price"),
+			rs.getString("image_url")
+		);
+	}
 
-    public long delete(long id) {
-        String sql = "DELETE FROM ITEM_INFO where id = ?";
-        jdbcTemplate.update(sql, id);
-        return id;
-    }
+	public long delete(long id) {
+		String sql = "DELETE FROM ITEM_INFO where id = ?";
+		jdbcTemplate.update(sql, id);
+		return id;
+	}
 }
