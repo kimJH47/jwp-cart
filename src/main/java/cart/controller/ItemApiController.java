86d37d324c47fd5c6575d7cec511ff78bd2c337f
kimJaeHyeon <kmr2644@gmail.com>
2023-05-21 07:35:54 +0900

    feat: item 전체 조회 api 구현

--- src/main/java/cart/controller/ItemApiController.java
+++ src/main/java/cart/controller/ItemApiController.java
@@ -1,13 +1,17 @@
 package cart.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cart.domain.Item;
 import cart.dto.request.ItemSaveRequest;
 import cart.dto.response.Response;
 import cart.service.ItemService;
@@ -27,5 +31,10 @@ public class ItemApiController {
 			.body(Response.createSuccessResponse(itemService.save(itemSaveRequest), "성공적으로 저장되었습니다."));
 	}
 
+	@GetMapping
+	public ResponseEntity<Response<List<Item>>> findAll() {
+		return ResponseEntity.ok()
+			.body(Response.createSuccessResponse(itemService.findAll(), "성공적으로 조회가 완료되었습니다"));
+	}
 
 }
