fe3a71bb9e4a7816e15a747c45f84c5050467e65
kimJaeHyeon <kmr2644@gmail.com>
2023-05-28 12:54:51 +0900

    feat: user 조회 서비스 구현

--- src/main/java/cart/service/UserService.java
+++ src/main/java/cart/service/UserService.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import cart.dto.response.UserSearchDto;
+import cart.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	public List<UserSearchDto> findAll() {
-		return null;
+		return userRepository.findAll();
 	}
 }
