37e679eb5686e825350397628fe3d7a317a2d9c2
kimJaeHyeon <kmr2644@gmail.com>
2023-06-04 17:12:31 +0900

    갸

--- src/main/java/cart/dto/response/UserSearchDto.java
+++ src/main/java/cart/dto/response/UserSearchDto.java
@@ -1,16 +1,16 @@
 package cart.dto.response;
 
 public class UserSearchDto {
-	private final String id;
+	private final String email;
 	private final String password;
 
-	public UserSearchDto(String id, String password) {
-		this.id = id;
+	public UserSearchDto(String email, String password) {
+		this.email = email;
 		this.password = password;
 	}
 
-	public String getId() {
-		return id;
+	public String getEmail() {
+		return email;
 	}
 
 	public String getPassword() {
