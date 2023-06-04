a60040fd9b24c98d4b80dd714837649cfb34cf05
kimJaeHyeon <kmr2644@gmail.com>
2023-06-04 18:15:55 +0900

    refactor: 장바구니 조회 api 스펙 변경

--- src/main/java/cart/controller/CartController.java
+++ src/main/java/cart/controller/CartController.java
@@ -1,13 +1,12 @@
 package cart.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cart.auth.interceptor.User;
+import cart.auth.resolver.annotation.Principal;
 import cart.dto.response.CartSearchDto;
 import cart.dto.response.Response;
 import cart.service.CartService;
@@ -22,9 +21,9 @@ public class CartController {
 		this.cartService = cartService;
 	}
 
-	@GetMapping("/{userId}")
-	public ResponseEntity<Response<List<CartSearchDto>>> findAll(@PathVariable String userId) {
+	@GetMapping
+	public ResponseEntity<Response<CartSearchDto>> findAll(@Principal User user) {
 		return ResponseEntity.ok()
-			.body(Response.createSuccessResponse(cartService.findByUserId(userId), "성공적으로 조회가 완료되었습니다."));
+			.body(Response.createSuccessResponse(cartService.findByUserEmail(user.getEmail()), "성공적으로 조회가 완료되었습니다."));
 	}
 }
