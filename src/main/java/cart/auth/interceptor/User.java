64136e1891e2870032ff2d4884912bf0f03bdfca
kimJaeHyeon <kmr2644@gmail.com>
2023-06-04 17:12:06 +0900

    feat: 유저의 인증정보가 담겨있는 객체 구현

---
+++ src/main/java/cart/auth/interceptor/User.java
@@ -0,0 +1,18 @@
+package cart.auth.interceptor;
+
+public class User {
+	private final String email;
+	private final String password;
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
