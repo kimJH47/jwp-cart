fb332541ba25ba5b05d43e75cb71b744cbf451c4
kimJaeHyeon <kmr2644@gmail.com>
2023-06-04 18:16:52 +0900

    feat: 유저 이메일로 장바구니 db 조회 기능 구현

---
+++ src/main/java/cart/repository/CartRepository.java
@@ -0,0 +1,44 @@
+package cart.repository;
+
+import java.util.ArrayList;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import cart.domain.Item;
+import cart.dto.response.CartSearchDto;
+
+@Repository
+public class CartRepository {
+
+	private final SimpleJdbcInsert simpleJdbcInsert;
+	private final JdbcTemplate jdbcTemplate;
+
+	public CartRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+		simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+			.withTableName("CART_INFO")
+			.usingGeneratedKeyColumns("ID");
+	}
+
+	public CartSearchDto findByUserEmail(String email) {
+		return jdbcTemplate.queryForObject("SELECT id,name,price,image_url FROM CART_INFO where email = ?",
+			carRowMapper(), email);
+	}
+
+	private RowMapper<CartSearchDto> carRowMapper() {
+		return (rs, rsNum) -> {
+			ArrayList<Item> items = new ArrayList<>();
+			while (rs.next()) {
+				items.add(
+					new Item(rs.getLong("id"),
+						rs.getString("name"),
+						rs.getInt("price"),
+						rs.getString("image_url")));
+			}
+			return new CartSearchDto(items.size(), items);
+		};
+	}
+}
