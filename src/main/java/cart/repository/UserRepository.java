6f252926d52079c1d9bcb6f2c0616c90fa77a819
kimJaeHyeon <kmr2644@gmail.com>
2023-06-04 17:16:09 +0900

    feat: user email 이 존재하는지와 비밀번호가 일치하는지 조회하는 메서드 구현

--- src/main/java/cart/repository/UserRepository.java
+++ src/main/java/cart/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package cart.repository;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,5 +29,19 @@ public class UserRepository {
 
 	private RowMapper<UserSearchDto> userRowMapper() {
 		return (rs, rowNum) -> new UserSearchDto(rs.getString("user_id"), rs.getString("password"));
+	}
+
+	public boolean isMatchEmailAndPassword(String userId, String password) {
+		if (!existUserId(userId)) {
+			return false;
+		}
+		return Objects.equals(jdbcTemplate.queryForObject("SELECT password FROM USER_INFO where userId = ? limit 1",
+			String.class, userId), password);
+	}
+
+	private boolean existUserId(String userId) {
+		String sql = "SELECT COUNT(user_id) as count FROM USER_INFO WHERE user_id = ? limit 1";
+		Integer count = jdbcTemplate.queryForObject(sql, Integer.class, userId);
+		return count != null && count == 1;
 	}
 }
