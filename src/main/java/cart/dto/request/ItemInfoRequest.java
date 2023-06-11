b5d22a05152d06543ceb7f505a861bbad73131cf
kimJaeHyeon <kmr2644@gmail.com>
2023-06-11 20:37:27 +0900

    feat: 카드 상품저장 request 구현

---
+++ src/main/java/cart/dto/request/ItemInfoRequest.java
@@ -0,0 +1,35 @@
+package cart.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+public class ItemInfoRequest {
+	@NotBlank(message = "상품의 이름은 필수입니다.")
+	private String name;
+	@NotBlank(message = "상품의 이미지는 필수 입니다.")
+	private String imageUrl;
+	@Positive(message = "상품의 가격은 0 보다 커야합니다.")
+	private int price;
+
+	public ItemInfoRequest() {
+	}
+
+	public ItemInfoRequest(String name, String imageUrl, int price) {
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+}
