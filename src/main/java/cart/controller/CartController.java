12a920a87717fafb9c89ab47f771f40034d6a38a
kimJaeHyeon <kmr2644@gmail.com>
2023-06-11 20:40:32 +0900

    feat: 카트저장 api 구현

--- src/main/java/cart/controller/CartController.java
+++ src/main/java/cart/controller/CartController.java
@@ -3,10 +3,12 @@ package cart.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cart.auth.interceptor.User;
 import cart.auth.resolver.annotation.Principal;
+import cart.dto.request.ItemInfoRequest;
 import cart.dto.response.CartSearchDto;
 import cart.dto.response.Response;
 import cart.service.CartService;
@@ -26,4 +28,12 @@ public class CartController {
 		return ResponseEntity.ok()
 			.body(Response.createSuccessResponse(cartService.findByUserEmail(user.getEmail()), "성공적으로 조회가 완료되었습니다."));
 	}
+
+	@PostMapping
+	public ResponseEntity<Response<CartSearchDto>> addItem(@Principal User user, ItemInfoRequest itemInfoRequest) {
+		return ResponseEntity.ok()
+			.body(Response.createSuccessResponse(cartService.addItem(user.getEmail(), itemInfoRequest),
+				"성공적으로 카트에 추가가 되었습니다."));
+	}
+
 }
